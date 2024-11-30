3c817cda8faf4fdf1eb499eabc99043c134bd175
Sirimongkol Dornsrikhot <Sirimongkol.dor@dome.tu.ac.th>
2024-12-01 00:44:50 +0700

    js form

--- script.js
+++ script.js
@@ -13,3 +13,60 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+// ส่วนของ form
+const form = document.getElementById('form');
+const entries = [];
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const fullname = document.getElementById('fullname').value.trim();
+  const gender = document.querySelector('input[name="gender"]:checked').value;
+  const email = document.getElementById('email').value.trim();
+  const satisfaction = document.getElementById('satisfaction').value;
+  const description = document.getElementById('description').value;
+
+  const isValid = true;
+  if (fullname.split(' ').length !== 2) {
+    alert('กรุณากรอกชื่อ-นามสกุลให้ถูกต้อง');
+    isValid = false;
+  }
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert('กรุณาใส่อีเมลล์ให้ถูกต้อง');
+    isValid = false;
+  }
+  if (!gender) {
+    alert('กรุณาเลือกเพศของคุณ');
+    isValid = false;
+  }
+  if (!satisfaction) {
+    alert('โปรดเลือกความพึงพอใจต่อเว็บไซต์');
+    isValid = false;
+  }
+
+  if (isValid) {
+    const newEntry = {
+      fullname,
+      gender,
+      email,
+      satisfaction,
+      description,
+    };
+    entries.push(newEntry);
+
+    const displayArea = document.getElementById('display-area');
+    const entryElement = document.createElement('div');
+    entryElement.innerHTML = `
+      <p><strong>ชื่อ-นามสกุล:</strong> ${newEntry.fullname}</p>
+      <p><strong>Gender:</strong> ${newEntry.gender}</p>
+      <p><strong>เพศ:</strong> ${newEntry.email}</p>
+      <p><strong>ความพึงพอใจต่อเว็บไซต์:</strong> ${newEntry.satisfaction}</p>
+      <p><strong>ข้อเสนอแนะ / ความคิดเห็น:</strong> ${newEntry.description}</p>
+    `;
+    displayArea.appendChild(entryElement);
+
+    form.reset();
+  }
+});
