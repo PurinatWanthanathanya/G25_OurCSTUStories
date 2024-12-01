096f2dcd6b20371e94f0bbc04417687b6aabfd9a
Kan <wuthikorn.boo@dome.tu.ac.th>
2024-12-01 16:28:16 +0700

    edit home.html and script.js

--- script.js
+++ script.js
@@ -74,3 +74,19 @@ form.addEventListener('submit', (event) => {
     form.reset();
   }
 });
+
+//swiper install//
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
