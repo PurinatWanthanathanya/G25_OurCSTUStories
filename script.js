959ff86754a5bdf79568badb21030ef98975f48b
Pooh <purinat.wan@dome.tu.ac.th>
2024-12-01 22:53:51 +0700

    add backtotop js file

--- script.js
+++ script.js
@@ -96,28 +96,3 @@ prev.onclick = function () {
   active = active - 1 >= 0 ? active - 1 : active;
   loadShow();
 }
-
-//function ตรวจว่าถึงตรงไหนแล้วให้โชว์ปุ่ม
-document.addEventListener('DOMContentLoaded', () => {
-  const backToTopButton = document.getElementById('back-to-top');
-
-  function toggleButtonVisibility() {
-    if (window.scrollY > 450) {
-      backToTopButton.style.display = 'block';
-    } else {
-      backToTopButton.style.display = 'none';
-    }
-  }
-  function scrollToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
-  }
-
-
-  window.addEventListener('scroll', toggleButtonVisibility);
-
-  backToTopButton.addEventListener('click', scrollToTop);
-  
-});
